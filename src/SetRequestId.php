eb8dce7384b7e5af2e0c6725386a5dd577485b4c
Dmytro Naidenko <dmitryi@stryber.com>
2020-07-02 13:48:20 +0300

    add SetRequestId middleware

---
+++ src/SetRequestId.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Stryber\Uuid;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\HttpFoundation\HeaderBag;
+
+class SetRequestId
+{
+    public const REQUEST_ID_HEADER = 'X-Request-ID';
+
+    private UuidInterface $uuid;
+
+    public function __construct(UuidGenerator $uuidGenerator)
+    {
+        $this->uuid = $uuidGenerator->generate();
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        $this->setHeader($request->headers);
+        /** @var Response $response */
+        $response = $next($request);
+        $this->setHeader($response->headers);
+
+        return $response;
+    }
+
+    private function setHeader(HeaderBag $headerBag): void
+    {
+        if ($headerBag->has(self::REQUEST_ID_HEADER)) {
+            return;
+        }
+
+        $headerBag->set(self::REQUEST_ID_HEADER, (string)$this->uuid);
+    }
+}
--- src/UuidServiceProvider.php
+++ src/UuidServiceProvider.php
@@ -4,13 +4,31 @@ declare(strict_types=1);
 
 namespace Stryber\Uuid;
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 final class UuidServiceProvider extends ServiceProvider
 {
+    private $middlewares = [
+        'requestId' => SetRequestId::class,
+    ];
+
     public function register(): void
+    {
+        $this->registerClasses();
+        $this->registerMiddlewares();
+    }
+
+    private function registerClasses(): void
     {
         $this->app->bind(UuidGenerator::class, PersistentUuidGenerator::class);
         $this->app->singleton(PersistentUuidGenerator::class, fn() => PersistentUuidGenerator::instance());
+    }
+
+    private function registerMiddlewares(): void
+    {
+        /** @var Router $router */
+        $router = $this->app['router'];
+        collect($this->middlewares)->each(fn($middleware, $alias) => $router->aliasMiddleware($alias, $middleware));
     }
 }
